acfc789bdab13bc08a69ad58a4c528f3287a23b4
Robert Fischer <artiefischer@gmail.com>
2018-12-12 08:06:50 -0700

    artie merge

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+  console.log("req.cookies:", req.cookies)
   res.send('respond with a resource');
 });
 
