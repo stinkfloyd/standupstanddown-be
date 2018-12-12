46baece0a08500762cf42793ea72029055e1191e
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-12 11:27:30 -0700

    "CORS Headers added."

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Middleware to initizilize passport & session
 app.use(passport.initialize())
 app.use(passport.session())
