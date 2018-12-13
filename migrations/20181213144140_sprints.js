f11c2dd327699e978dcf13319d10a8573f8a3a50
Robert Fischer <artiefischer@gmail.com>
2018-12-13 14:53:23 -0700

    sprints and dailies tables

---
+++ migrations/20181213144140_sprints.js
@@ -0,0 +1,14 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('sprints', function (table) {
+    // TABLE COLUMN DEFINITIONS HERE
+    table.increments() // This is the id, auto-increments
+    table.integer('sprint_length').notNullable().defaultTo(5)
+    table.string('sprint_goal', 255).notNullable().defaultTo('')
+    table.string('notes', 400).notNullable().defaultTo('')
+    table.integer('team_id').notNullable()
+    table.foreign(`team_id`).references(`teams.id`).onDelete(`CASCADE`)
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = (knex, Promise) => knex.schema.dropTableIfExists('sprints')
