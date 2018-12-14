1397a81df2a879fe9627803e6d9cb63d03145275
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-13 21:24:44 -0700

    "Verifications"

--- migrations/20181213144847_dailies.js
+++ migrations/20181213144847_dailies.js
@@ -1,13 +1,13 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('dailies', (table) => {
-  table.increments() // This is the id, auto-increments
-  table.string('yesterday', 255).notNullable().defaultTo('')
-  table.string('today', 255).notNullable().defaultTo('')
-  table.string('helps', 255).notNullable().defaultTo('')
-  table.integer('user_id').notNullable()
-  table.foreign(`user_id`).references(`users.id`).onDelete(`CASCADE`)
-  table.integer('sprint_id').notNullable()
-  table.foreign(`sprint_id`).references(`sprint.id`).onDelete(`CASCADE`)
+    table.increments() // This is the id, auto-increments
+    table.string('yesterday', 255).notNullable().defaultTo('')
+    table.string('today', 255).notNullable().defaultTo('')
+    table.string('helps', 255).notNullable().defaultTo('')
+    table.integer('user_id').notNullable()
+    table.foreign(`user_id`).references(`users.id`).onDelete(`CASCADE`)
+    table.integer('sprint_id').notNullable()
+    table.foreign(`sprint_id`).references(`sprints.id`).onDelete(`CASCADE`)
   })
 }
 
--- routes/teams.js
+++ routes/teams.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const teamModel = require('../models/teamsModel.js')
+const teams_usersModel = require('../models/teams_usersModel.js')
 const jwt = require('jsonwebtoken')
 
 // Middleware Functions
@@ -23,13 +24,23 @@ const jwtVerify = (req, res, next) => {
   jwt.verify(req.cookies.token, process.env.TOKEN_SECRET, (err, _payload) => {
     if (err) {
       err.status = 401
-      err.message = `Unauthorized`
+      err.message = `Unauthorized - Bad JWT Token cookie`
       return next(err);
     } else {
       req.payload = _payload
       next()
     }
   })
+}
+
+const addOwnerToTeam = async (req, res, next) => {
+  console.log('req.team(response) in addOWner ', req.team)
+  let creator = {
+    team_id: req.team.id,
+    user_id: req.team.creator_id,
+  }
+  teams_usersModel.create(creator)
+    .then(response => res.send(response))
 }
 
 // GET ALL TEAMS -> COMMENT OUT FOR PRODUCTION
@@ -56,26 +67,29 @@ router.get('/:id', verifyId, jwtVerify, (req, res, next) => {
 })
 
 // POST A TEAM TO THE DATABASE
-router.post('/', jwtVerify, (req, res, next) => {
+router.post('/', (req, res, next) => {
   // Create the initial team
   let newTeam = {
     name: req.body.name,
     creator_id: req.body.creator_id,
   }
   teamModel.create(newTeam)
-    .then(response => { console.log(response); res.send(response) })
+    .then((response) => {
+      console.log('response: ', response)
+      if (!response) {
+        return next(response)
+      } else {
+        req.team = response
+        addOwnerToTeam(req, res, next)
+      }
+    })
     .catch(err => next(err))
-
-  /*
-   *  TODO:: NEED TO ADD CREATOR TO THE TEAMS_USERS
-   */
 })
 
 // DELETE A TEAM
 router.delete('/:id', verifyId, jwtVerify, (req, res, next) => {
   teamModel.getOneTeam(req.params.id, next)
     .then((response) => {
-      console.log("response: ", response);
       if (response.creator_id === req.payload.id) {
         teamModel.deleteOne(req.params.id)
           .then(response => res.send(response))
@@ -107,7 +121,7 @@ router.put('/:id', verifyId, jwtVerify, async (req, res, next) => {
       let newTeam = { ...team, name: req.body.name }
       teamModel.editName(req.params.id, newTeam)
         .then(response => res.send(response))
-        .catch(err => Promise.reject(err))
+        .catch(err => next(err))
     }
   }
 })
