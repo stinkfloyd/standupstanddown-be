f07026c30f911631a77a67d30825cce861ee010e
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-18 15:47:30 -0700

    "More heroku fun"

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://standup-be.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", "https://standup-be.herokuapp.com");
   res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Credentials", "true")
