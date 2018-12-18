8d429e5edcb2e994b8570a79c1e224122f87e20e
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-18 14:20:00 -0700

    "Backend setup for serving front end"

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.get('/github/callback', passport.authenticate('github'), (req, res) => {
   res.cookie("token", token, {
     expires: new Date(Date.now() + 900000 * 4 * 24) // 24 hours
   })
-  res.redirect('http://standup-be.herokuapp.com/');
+  res.redirect('http://localhost:3000/');
 });
 
 module.exports = router;
