ff0628927cfa3c2c7c02f58693152197bb26dda7
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-18 14:29:40 -0700

    "Working seperately, able to be combined"

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.get('/github/callback', passport.authenticate('github'), (req, res) => {
   res.cookie("token", token, {
     expires: new Date(Date.now() + 900000 * 4 * 24) // 24 hours
   })
-  res.redirect('http://localhost:3000/');
+  res.redirect('http://localhost:8080/');
 });
 
 module.exports = router;
